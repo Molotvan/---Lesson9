60f1184d9a33fe2cf10b00f78a41834ece65f3c8
Molotvan <molotvan@yandex.ru>
2022-09-08 20:11:04 +0300

    init

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿int M = 1;
+int N = 15;
+Console.WriteLine(Sum(M, N));
+
+int Sum(int m, int n)
+{
+    int sum = 0;
+    for (int i = m; i <= N; i++)
+    {
+        sum = sum + i;
+    }
+    return sum;
+}
---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿int M = 1;
+int N = 13;
+PrintNumbers(M, N);
+
+void PrintNumbers(int m, int n)
+{
+    if (m <= n)
+    {
+        Console.WriteLine(m);
+        m++;
+        PrintNumbers(m, n);
+    }
+}
